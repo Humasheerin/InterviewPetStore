5eff6118047692bcdf32b30cf94f8a0192af3490
Humasheerin <48506980+Humasheerin@users.noreply.github.com>
2021-04-15 16:02:52 +0530

    Update data.sql

--- store/src/main/resources/data.sql
+++ store/src/main/resources/data.sql
@@ -1,2 +1,5 @@
 insert into Pet values('Dog',1000,'German');
 insert into Pet values('Cat',51000,'Persian');
+
+insert into Pet values('Hen',3000,'Rock');
+insert into Pet values('Rabbit',4000,'Dutch Lop');
